4b8a9dca643b1ee46fe3d3a9010054793db90e8e
Nick Cosentino <n.b.cosentino@gmail.com>
2015-01-31 12:53:20 -0500

    Collisions and Simple Pre-defined Map

--- Assets/Scripts/Actors/ActorMovementBehaviour.cs
+++ Assets/Scripts/Actors/ActorMovementBehaviour.cs
@@ -2,10 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Assets.Scripts.Actors;
 using UnityEngine;
 
-namespace Assets.Scripts
+namespace Assets.Scripts.Actors
 {
     public class ActorMovementBehaviour : MonoBehaviour, IActorMovementBehaviour
     {
---
+++ Assets/Scripts/MapLoader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class MapLoader : MonoBehaviour
+    {
+        #region Fields
+        private object _map;
+        #endregion
+
+        #region Methods
+        public void Start()
+        {
+            ////using (var reader = XmlReader.Create(new StringReader(Resources.Load<TextAsset>("Maps/Swamp").text)))
+            ////{
+                //// TODO: fill this out!
+            ////}
+        }
+        #endregion
+    }
+}
