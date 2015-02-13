7ca4b37a0b28bdd8fde156261807825bfe7c2592
Nick Cosentino <n.b.cosentino@gmail.com>
2015-02-12 23:36:49 -0500

    Drag + Drop Between Equipment & Inventory
    - It's still rough (because inventories don't have indexing actually setup yet) but you can totally move things around now.

--- Assets/Scripts/Gui/Inventory/InventoryGridViewPanelBehaviour.cs
+++ Assets/Scripts/Gui/Inventory/InventoryGridViewPanelBehaviour.cs
@@ -1,9 +1,137 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using ProjectXyz.Application.Interface.Items;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
 
-public class InventoryBehaviour : MonoBehaviour
+namespace Assets.Scripts.Gui.Inventory
 {
-    #region Unity Properties
+    public class InventoryGridViewPanelBehaviour : MonoBehaviour
+    {
+        #region Fields
+        private IInventory _inventory;
+        #endregion
 
-    #endregion
+        #region Unity Properties
+        public ScrollRect ItemCollectionScrollRect;
+
+        public GameObject InventorySlotPrefab;
+        #endregion
+
+        #region Properties
+        public IInventory Inventory
+        {
+            get
+            {
+                return _inventory;
+            }
+
+            set
+            {
+                UnhookInventoryEvents(_inventory);
+                _inventory = value;
+                HookInventoryEvents(_inventory);
+            }
+        }
+        #endregion
+
+        #region Methods
+        public void OnDestroy()
+        {
+            Inventory = null;
+        }
+
+        public void Start()
+        {
+            var mutator = ProjectXyz.Application.Core.Items.Inventory.Create();
+            Inventory = mutator;
+            mutator.ItemCapacity = 10;
+        }
+
+        private void HookInventoryEvents(IInventory inventory)
+        {
+            if (inventory == null)
+            {
+                return;
+            }
+
+            inventory.CapacityChanged += Inventory_CapacityChanged;
+            inventory.CollectionChanged += Inventory_CollectionChanged;
+        }
+
+        private void UnhookInventoryEvents(IInventory inventory)
+        {
+            if (inventory == null)
+            {
+                return;
+            }
+
+            inventory.CapacityChanged -= Inventory_CapacityChanged;
+            inventory.CollectionChanged -= Inventory_CollectionChanged;
+        }
+
+        private void PopulateItemSlots(IMutableInventory inventory, ScrollRect itemCollectionScrollRect)
+        {
+            (from
+                 Transform child 
+             in 
+                 itemCollectionScrollRect.transform
+             select 
+                child.gameObject)
+            .ToList()
+            .ForEach(Destroy);
+            
+            if (inventory == null)
+            {
+                return;
+            }
+
+            const int ROW_COUNT = 10;
+            const int COLUMN_COUNT = 10;
+
+            float slotWidth = itemCollectionScrollRect.content.rect.width / ROW_COUNT;
+            float slotHeight = itemCollectionScrollRect.content.rect.height / COLUMN_COUNT;
+
+            for (int x = 0; x < COLUMN_COUNT; x++)
+            {
+                for (int y = 0; y < ROW_COUNT; y++)
+                {
+                    var slot = (GameObject)PrefabUtility.InstantiatePrefab(InventorySlotPrefab);
+                    slot.transform.SetParent(itemCollectionScrollRect.content.transform);
+
+                    var slotRectTransform = slot.GetComponent<RectTransform>();
+                    slotRectTransform.SetInsetAndSizeFromParentEdge(
+                        RectTransform.Edge.Left, 
+                        x * slotWidth, 
+                        0);
+                    slotRectTransform.SetInsetAndSizeFromParentEdge(
+                        RectTransform.Edge.Top,
+                        y * slotHeight,
+                        0);
+                    slotRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, slotWidth);
+                    slotRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, slotHeight);
+
+                    var inventorySlotBehaviour = (InventorySlotBehaviourBehaviour)slot.GetComponent(typeof(InventorySlotBehaviourBehaviour));
+                    inventorySlotBehaviour.Inventory = inventory;
+                    inventorySlotBehaviour.InventoryIndex = x * COLUMN_COUNT + y;
+                }
+            }
+        }
+        #endregion
+
+        #region Event Handlers
+        private void Inventory_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            PopulateItemSlots((IMutableInventory)sender, ItemCollectionScrollRect);
+        }
+
+        private void Inventory_CapacityChanged(object sender, EventArgs e)
+        {
+            PopulateItemSlots((IMutableInventory)sender, ItemCollectionScrollRect);
+        }
+        #endregion
+    }
 }
---
+++ Assets/Scripts/Gui/Inventory/InventorySlotBehaviourBehaviour.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Scripts.Actors.Player;
+using Assets.Scripts.Scenes;
+using ProjectXyz.Application.Core.Enchantments;
+using ProjectXyz.Application.Core.Items;
+using ProjectXyz.Application.Interface.Items;
+using ProjectXyz.Application.Interface.Items.ExtensionMethods;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts.Gui.Inventory
+{
+    public class InventorySlotBehaviourBehaviour : MonoBehaviour, ICanAddItemBehaviour, ICanRemoveItemBehaviour, IHasItemBehaviour
+    {
+        #region Fields
+        private IExploreSceneManager _exploreSceneManager;
+        private ICanEquip _equippable;
+        private ICanUnequip _unequippable;
+        private Func<string, IItem> _getItemForSlotCallback;
+        #endregion
+        
+        #region Properties
+        public int InventoryIndex { get; set; }
+
+        public IMutableInventory Inventory { get; set; }
+
+        public IItem Item
+        {
+            get { return Inventory.Items.ToList()[InventoryIndex]; }
+        }
+        #endregion
+
+        #region Methods
+        public void Start()
+        {
+        }
+
+        public void OnDestroy()
+        {
+           
+        }
+
+        public bool CanRemoveItem()
+        {
+            var result = Inventory.Items.Count > InventoryIndex;
+            Debug.Log(string.Format("Can remove from {0}? {1}", InventoryIndex, result));
+
+            return result;
+        }
+
+        public IItem RemoveItem()
+        {
+            var item = Item;
+            Inventory.Remove(Item);
+            Debug.Log(string.Format("Removed {0} from {1}.", item, InventoryIndex));
+
+            return item;
+        }
+
+        public bool CanAddItem(IItem item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("Cannot add null item.");
+            }
+
+            var result = Inventory.Items.Count < Inventory.ItemCapacity;
+            Debug.Log(string.Format("Can {0} be added? {1}", item, result));
+
+            return result;
+        }
+
+        public void AddItem(IItem item)
+        {
+            Inventory.Add(item);
+            Debug.Log(string.Format("Added {0}.", item));
+        }
+        #endregion
+    }
+}
