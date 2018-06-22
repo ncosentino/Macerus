e97e3a1282528bc570fbbfb45f2e407b60c932e4
ncosentino <n.b.cosentino@gmail.com>
2018-06-22 01:45:25 -0400

    Players Get Inventory and Belt Item Containers

--- Macerus.Plugins.Features.GameObjects.Actors/AdditionalActorBehaviorsProvider.cs
+++ Macerus.Plugins.Features.GameObjects.Actors/AdditionalActorBehaviorsProvider.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using Macerus.Shared.Behaviors;
 using ProjectXyz.Api.Behaviors;
 using ProjectXyz.Api.GameObjects;
+using ProjectXyz.Plugins.Features.CommonBehaviors;
 using ProjectXyz.Plugins.Features.GameObjects.Actors.Api;
+using ProjectXyz.Shared.Framework;
 
 namespace Macerus.Plugins.Features.GameObjects.Actors
 {
@@ -13,6 +15,8 @@ namespace Macerus.Plugins.Features.GameObjects.Actors
         {
             yield return new WorldLocationBehavior();
             yield return new MovementBehavior();
+            yield return new ItemContainerBehavior(new StringIdentifier("Inventory"));
+            yield return new ItemContainerBehavior(new StringIdentifier("Belt"));
         }
     }
 }
