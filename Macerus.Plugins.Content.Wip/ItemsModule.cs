05016897d8aa110ef264be094167ebb62a3b124e
ncosentino <n.b.cosentino@gmail.com>
2018-12-07 13:54:35 -0500

    WIP some generator stuff for equippable items

--- Macerus.Plugins.Content.Wip/ItemsModule.cs
+++ Macerus.Plugins.Content.Wip/ItemsModule.cs
@@ -5,8 +5,10 @@ using System.Linq;
 using Autofac;
 using Macerus.Plugins.Features.GameObjects.Items.Behaviors;
 using ProjectXyz.Api.Behaviors;
+using ProjectXyz.Api.Framework;
 using ProjectXyz.Api.GameObjects.Generation;
 using ProjectXyz.Api.GameObjects.Generation.Attributes;
+using ProjectXyz.Plugins.Features.CommonBehaviors;
 using ProjectXyz.Plugins.Features.GameObjects.Items.Generation.InMemory;
 using ProjectXyz.Plugins.Features.GameObjects.Items.Generation;
 using ProjectXyz.Shared.Framework;
@@ -34,9 +36,10 @@ namespace Macerus.Plugins.Content.Wip
                                     new StringGeneratorAttributeValue("normal"),
                                     true),
                             },
-                            new[]
+                            new IGeneratorComponent[]
                             {
-                               new NameGeneratorComponent("Normal Item"),  
+                               new NameGeneratorComponent("Normal Item"),
+                               new EquippableGeneratorComponent(new[] { new StringIdentifier("body") }), 
                             }),
                         new ItemDefinition(
                             new[]
@@ -62,6 +65,33 @@ namespace Macerus.Plugins.Content.Wip
                 .RegisterType<NameGeneratorComponentToBehaviorConverter>()
                 .AsImplementedInterfaces()
                 .SingleInstance();
+            builder
+                .RegisterType<EquippableGeneratorComponentToBehaviorConverter>()
+                .AsImplementedInterfaces()
+                .SingleInstance();
+        }
+    }
+
+    public sealed class EquippableGeneratorComponent : IGeneratorComponent
+    {
+        public EquippableGeneratorComponent(IEnumerable<IIdentifier> allowedEquipSlots)
+        {
+            AllowedEquipSlots = allowedEquipSlots.ToArray();
+        }
+
+        public IEnumerable<IGeneratorAttribute> SupportedAttributes { get; } = Enumerable.Empty<IGeneratorAttribute>();
+
+        public IReadOnlyCollection<IIdentifier> AllowedEquipSlots { get; }
+    }
+
+    public sealed class EquippableGeneratorComponentToBehaviorConverter : IDiscoverableGeneratorComponentToBehaviorConverter
+    {
+        public Type ComponentType { get; } = typeof(EquippableGeneratorComponent);
+
+        public IEnumerable<IBehavior> Convert(IGeneratorComponent generatorComponent)
+        {
+            var equippableGeneratorComponent = (EquippableGeneratorComponent)generatorComponent;
+            yield return new CanBeEquippedBehavior(equippableGeneratorComponent.AllowedEquipSlots);
         }
     }
 
