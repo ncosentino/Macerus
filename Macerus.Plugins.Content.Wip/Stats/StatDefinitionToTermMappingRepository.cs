6512ee0ff42296a808cc3c4d2ae81cb4401eb015
ncosentino <n.b.cosentino@gmail.com>
2021-02-06 09:56:50 -0800

    RPG-87: Change Enchantment Definition Repository Consumption to use ReadOnly
    - Updated repositories

--- Macerus.Plugins.Content.Wip/Stats/StatDefinitionToTermMappingRepository.cs
+++ Macerus.Plugins.Content.Wip/Stats/StatDefinitionToTermMappingRepository.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Globalization;
 
 using ProjectXyz.Api.Data.Databases;
 using ProjectXyz.Api.Stats;
@@ -11,16 +11,64 @@ namespace Macerus.Plugins.Content.Wip.Stats
     public sealed class StatDefinitionToTermMappingRepository : IStatDefinitionToTermMappingRepository
     {
         private readonly IConnectionFactory _connectionFactory;
-        private readonly Lazy<IReadOnlyCollection<IStatDefinitionToTermMapping>> _lazyCache;
+        private readonly List<IStatDefinitionToTermMapping> _definitionCache;
+
+        private bool _dirtyCache;
 
         public StatDefinitionToTermMappingRepository(IConnectionFactory connectionFactory)
         {
             _connectionFactory = connectionFactory;
-            _lazyCache = new Lazy<IReadOnlyCollection<IStatDefinitionToTermMapping>>(() =>
-                ReadAll().ToArray());
+            _definitionCache = new List<IStatDefinitionToTermMapping>();
+            _dirtyCache = true;
         }        
 
-        public IEnumerable<IStatDefinitionToTermMapping> GetStatDefinitionIdToTermMappings() => _lazyCache.Value;
+        public IEnumerable<IStatDefinitionToTermMapping> GetStatDefinitionIdToTermMappings()
+        {
+            if (_dirtyCache)
+            {
+                _definitionCache.Clear();
+                _definitionCache.AddRange(ReadAll());
+                _dirtyCache = false;
+            }
+
+            return _definitionCache;
+        }
+
+        public void WriteStatDefinitionIdToTermMappings(IEnumerable<IStatDefinitionToTermMapping> statDefinitionToTermMappings)
+        {
+            using (var connection = _connectionFactory.OpenNew())
+            using (var transaction = connection.BeginTransaction())
+            {
+                foreach (var statDefinitionToTermMapping in statDefinitionToTermMappings)
+                {
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = @"
+                        INSERT INTO
+                            stat_definitions
+                        SET
+                            id=@id,
+                            term=@term
+                        ;";
+                        var id = Convert.ToInt32(
+                            statDefinitionToTermMapping.StatDefinitionId.ToString(),
+                            CultureInfo.InvariantCulture);
+                        command.AddParameter("@id", id);
+                        command.AddParameter("@term", statDefinitionToTermMapping.Term);
+
+                        var result = command.ExecuteNonQuery();
+                        if (result != 1)
+                        {
+                            throw new InvalidOperationException(
+                                $"The stat definition to term mapping could not be added. Result code {result}.");
+                        }
+                    }
+                }
+
+                transaction.Commit();
+                _dirtyCache = true;
+            }
+        }
 
         private IEnumerable<IStatDefinitionToTermMapping> ReadAll()
         {
