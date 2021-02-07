6a3fcb45ccb03193c5854380d903bb37c3762b87
ncosentino <n.b.cosentino@gmail.com>
2021-02-06 23:42:18 -0800

    Unity Cannot Work w/ localhost
    - Throws a socket exception. I suspect under the hood it resolves to an
      IPv6 address or something.

--- Macerus.Plugins.Data.MySql/MySqlConnectionFactory.cs
+++ Macerus.Plugins.Data.MySql/MySqlConnectionFactory.cs
@@ -11,7 +11,7 @@ namespace Macerus.Plugins.Features.Data.MySql
         public IDbConnection Create()
         {
             var connection = new MySqlConnection(
-                $"Server=localhost;" +
+                $"Server=127.0.0.1;" +
                 $"Database=macerus;" +
                 $"Uid=macerus;" +
                 $"Pwd=macerus;");
