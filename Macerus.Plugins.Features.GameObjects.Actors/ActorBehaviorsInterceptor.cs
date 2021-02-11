2dc7a5e34f793c190391d1cd7d13fe6a4a6396a5
ncosentino <n.b.cosentino@gmail.com>
2021-02-10 23:06:25 -0800

    Bring Back Test Light Radius :)

--- Macerus.Plugins.Features.GameObjects.Actors/ActorBehaviorsInterceptor.cs
+++ Macerus.Plugins.Features.GameObjects.Actors/ActorBehaviorsInterceptor.cs
@@ -2,24 +2,31 @@
 
 using ProjectXyz.Api.Behaviors;
 using ProjectXyz.Api.GameObjects;
+using ProjectXyz.Api.Stats;
 using ProjectXyz.Plugins.Features.CommonBehaviors.Api;
 using ProjectXyz.Plugins.Features.GameObjects.Actors.Api;
-using ProjectXyz.Shared.Framework;
 
 namespace Macerus.Plugins.Features.GameObjects.Actors
 {
     public sealed class ActorBehaviorsInterceptor : IActorBehaviorsInterceptor
     {
+        private readonly IReadOnlyStatDefinitionToTermMappingRepository _statDefinitionToTermMappingRepository;
+
+        public ActorBehaviorsInterceptor(IReadOnlyStatDefinitionToTermMappingRepository statDefinitionToTermMappingRepository)
+        {
+            _statDefinitionToTermMappingRepository = statDefinitionToTermMappingRepository;
+        }
+
         public void Intercept(ICollection<IBehavior> behaviors)
         {
             var mutableStats = behaviors.GetOnly<IHasMutableStatsBehavior>();
             mutableStats.MutateStats(stats =>
             {
-                stats[new StringIdentifier("Light Radius Radius")] = 10;
-                stats[new StringIdentifier("Light Radius Intensity")] = 0.5;
-                stats[new StringIdentifier("Light Radius Red")] = 1;
-                stats[new StringIdentifier("Light Radius Green")] = 0;
-                stats[new StringIdentifier("Light Radius Blue")] = 1;
+                stats[_statDefinitionToTermMappingRepository.GetStatDefinitionToTermMappingByTerm("LIGHT_RADIUS_RADIUS").StatDefinitionId] = 10;
+                stats[_statDefinitionToTermMappingRepository.GetStatDefinitionToTermMappingByTerm("LIGHT_RADIUS_INTENSITY").StatDefinitionId] = 0.5;
+                stats[_statDefinitionToTermMappingRepository.GetStatDefinitionToTermMappingByTerm("LIGHT_RADIUS_RED").StatDefinitionId] = 1;
+                stats[_statDefinitionToTermMappingRepository.GetStatDefinitionToTermMappingByTerm("LIGHT_RADIUS_GREEN").StatDefinitionId] = 0;
+                stats[_statDefinitionToTermMappingRepository.GetStatDefinitionToTermMappingByTerm("LIGHT_RADIUS_BLUE").StatDefinitionId] = 1;
             });
         }
     }
