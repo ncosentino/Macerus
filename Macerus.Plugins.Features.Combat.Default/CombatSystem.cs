8b1aabab47265ce645e137a4ac47bdce0eaf40ff
ncosentino <n.b.cosentino@gmail.com>
2021-04-01 09:22:21 -0700

    Tick Combat System Turns

--- Macerus.Plugins.Features.Combat.Default/CombatSystem.cs
+++ Macerus.Plugins.Features.Combat.Default/CombatSystem.cs
@@ -2,20 +2,53 @@
 using System.Collections.Generic;
 
 using ProjectXyz.Api.Behaviors;
+using ProjectXyz.Api.Behaviors.Filtering;
+using ProjectXyz.Api.Framework.Entities;
 using ProjectXyz.Api.Systems;
+using ProjectXyz.Plugins.Features.Combat.Api;
+using ProjectXyz.Plugins.Features.TurnBased.Api;
 
 namespace Macerus.Plugins.Features.Combat.Default
 {
     public sealed class CombatSystem : IDiscoverableSystem
     {
+        private readonly ICombatTurnManager _combatTurnManager;
+        private readonly IFilterContextProvider _filterContextProvider;
+
+        public CombatSystem(
+            ICombatTurnManager combatTurnManager,
+            IFilterContextProvider filterContextProvider)
+        {
+            _combatTurnManager = combatTurnManager;
+            _filterContextProvider = filterContextProvider;
+        }
+
         public int? Priority => null;
 
         public void Update(
             ISystemUpdateContext systemUpdateContext,
             IEnumerable<IHasBehaviors> hasBehaviors)
         {
-            // FIXME: if we're in combat, we need to check elapsed turns and
-            // tell the combat manager to explicitly update
+            var turnInfo = systemUpdateContext
+                .GetFirst<IComponent<ITurnInfo>>()
+                .Value;
+            if (!InCombat(turnInfo))
+            {
+                return;
+            }
+
+            var filterContext = _filterContextProvider.GetContext();
+            _combatTurnManager.ProgressTurn(
+                filterContext,
+                1);
+        }
+
+        private bool InCombat(ITurnInfo turnInfo)
+        {
+            // FIXME: we need a better way to check if we're in combat
+            return
+                turnInfo.ElapsedTurns == 1 &&
+                !turnInfo.GlobalSync;
         }
     }
 }
