bdbec581fcaa4c212566ad5a3a5d2ee713f9314e
Nick Cosentino <n.b.cosentino@gmail.com>
2021-05-04 20:44:14 -0700

    Serialization Enhancements
    - Some super primitive tests for serializing... these need real assertions.
    - Added more shortened serialization ids.

--- Macerus.Plugins.Content/Data/SerializationModule.cs
+++ Macerus.Plugins.Content/Data/SerializationModule.cs
@@ -5,13 +5,21 @@ using System.Linq;
 using Autofac;
 
 using Macerus.Plugins.Features.GameObjects.Enchantments;
+using Macerus.Plugins.Features.Mapping.TiledNet;
 
 using ProjectXyz.Api.Behaviors.Filtering.Attributes;
+using ProjectXyz.Api.Framework;
+using ProjectXyz.Api.GameObjects;
+using ProjectXyz.Api.GameObjects.Behaviors;
 using ProjectXyz.Api.GameObjects.Generation;
 using ProjectXyz.Framework.Autofac;
+using ProjectXyz.Plugins.Features.Behaviors.Default;
 using ProjectXyz.Plugins.Features.Behaviors.Filtering.Default.Attributes;
+using ProjectXyz.Plugins.Features.CommonBehaviors;
 using ProjectXyz.Plugins.Features.GameObjects.Enchantments.Default.Calculations;
 using ProjectXyz.Plugins.Features.GameObjects.Generation.Default;
+using ProjectXyz.Plugins.Features.Mapping.Api;
+using ProjectXyz.Plugins.Features.Mapping.Default;
 using ProjectXyz.Shared.Data.Serialization;
 using ProjectXyz.Shared.Framework;
 
@@ -36,11 +44,29 @@ namespace Macerus.Plugins.Content.Data
                         [typeof(StringFilterAttributeValue)] = "StringFilterAttributeValue",
                         [typeof(RangeFilterAttributeValue)] = "RangeFilterAttributeValue",
                         [typeof(CalculationPriority<int>)] = "IntCalculationPriority",
+                        [typeof(IdentifierBehavior)] = "IdentifierBehavior",
                     };
+                    mapping.AddRange(WithCollectionMappings<TileResourceComponent>("TileResourceComponent"));
+                    mapping.AddRange(WithCollectionMappings<MapLayersBehavior>("MapLayersBehavior"));
+                    mapping.AddRange(WithCollectionMappings<MapLayer>("MapLayer"));
+                    mapping.AddRange(WithCollectionMappings<MapTile>("MapTile"));
                     mapping.AddRange(WithCollectionMappings<StringIdentifier>("sid"));
                     mapping.AddRange(WithCollectionMappings<IntIdentifier>("iid"));
+                    mapping.AddRange(WithCollectionMappings<GameObject>("obj"));
+                    mapping.AddRange(WithCollectionMappings<IMapLayer>("IMapLayer"));
+                    mapping.AddRange(WithCollectionMappings<ITileComponent>("ITileComponent"));
+                    mapping.AddRange(WithCollectionMappings<IMapTile>("IMapTile"));
+                    mapping.AddRange(WithCollectionMappings<IIdentifier>("id"));
+                    mapping.AddRange(WithCollectionMappings<IGameObject>("iobj"));
+                    mapping.AddRange(WithCollectionMappings<IBehavior>("IBehavior"));
                     mapping.AddRange(WithCollectionMappings<IFilterAttribute>("IFilterAttribute"));
                     mapping.AddRange(WithCollectionMappings<IGeneratorComponent>("IGeneratorComponent"));
+                    mapping.AddRange(WithCollectionMappings<int>("int"));
+                    mapping.AddRange(WithCollectionMappings<double>("dbl"));
+                    mapping.AddRange(WithCollectionMappings<float>("flt"));
+                    mapping.AddRange(WithCollectionMappings<string>("str"));
+                    mapping.AddRange(WithCollectionMappings<bool>("bool"));
+                    mapping.AddRange(WithCollectionMappings<byte>("byt"));
                     var serializableIdShorterner = new SerializableIdShorterner(mapping);
                     return serializableIdShorterner;
                 })
@@ -56,6 +82,9 @@ namespace Macerus.Plugins.Content.Data
             yield return new KeyValuePair<Type, string>(
                 typeof(T[]),
                 $"{shortened}[Array]");
+            yield return new KeyValuePair<Type, string>(
+                typeof(List<T>),
+                $"{shortened}[List]");
             yield return new KeyValuePair<Type, string>(
                 typeof(IEnumerable<T>),
                 $"{shortened}[Enumerable]");
---
+++ Macerus.Tests/Plugins/Features/GameObjects/GameObjectSerializationTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Macerus.Api.Behaviors;
+using Macerus.Api.Behaviors.Filtering;
+using Macerus.Api.GameObjects;
+using Macerus.Plugins.Features.Encounters;
+
+using ProjectXyz.Api.Data.Serialization;
+using ProjectXyz.Api.Framework;
+using ProjectXyz.Api.GameObjects;
+using ProjectXyz.Plugins.Features.GameObjects.Actors.Api;
+using ProjectXyz.Plugins.Features.Mapping.Api;
+using ProjectXyz.Shared.Framework;
+
+using Xunit;
+
+namespace Macerus.Tests.Plugins.Features.GameObjects.Actors
+{
+    public sealed class GameObjectSerializationTests
+    {
+        private static readonly MacerusContainer _container;
+        private static readonly TestAmenities _testAmenities;
+        private static readonly ISerializer _serializer;
+        private static readonly IDeserializer _deserializer;
+
+        static GameObjectSerializationTests()
+        {
+            _container = new MacerusContainer();
+            _testAmenities = new TestAmenities(_container);
+            _serializer = _container.Resolve<ISerializer>();
+            _deserializer = _container.Resolve<IDeserializer>();
+        }
+
+        [Fact]
+        private void SerializeAndDeserialize_SwampMap_ExpectedResults()
+        {
+            _testAmenities.UsingCleanMapAndObjects(() =>
+            {
+                var mapManager = _container.Resolve<IMapManager>();
+                mapManager.SwitchMap(new StringIdentifier("swamp"));
+
+                var input = mapManager.ActiveMap;
+                var result = Exercise(input);
+
+                Assert.NotNull(result.Item1);
+                Assert.False(
+                    string.IsNullOrWhiteSpace(result.Item2),
+                    "Serialized json was null or whitespace.");
+                Assert.Equal(
+                    input.Behaviors.Count,
+                    result.Item1.Behaviors.Count);
+            });
+        }
+
+        [Fact]
+        private void SerializeAndDeserialize_Player_ExpectedResults()
+        {
+            _testAmenities.UsingCleanMapAndObjects(() =>
+            {
+                var mapManager = _container.Resolve<IMapManager>();
+                var mapGameObjectManager = _container.Resolve<IReadOnlyMapGameObjectManager>();
+
+                // FIXME: this is just to get the player
+                mapManager.SwitchMap(new StringIdentifier("swamp"));
+                var player = mapGameObjectManager
+                    .GameObjects
+                    .Single(x => x.Has<IPlayerControlledBehavior>());
+
+                var result = Exercise(player);
+
+                Assert.NotNull(result.Item1);
+                Assert.False(
+                    string.IsNullOrWhiteSpace(result.Item2),
+                    "Serialized json was null or whitespace.");
+                Assert.Equal(
+                    player.Behaviors.Count,
+                    result.Item1.Behaviors.Count);
+            });
+        }
+
+        private Tuple<IGameObject, string> Exercise(IGameObject gameObjectToSerialize)
+        {
+            using (var stream = new MemoryStream())
+            {
+                _serializer.Serialize(
+                    stream,
+                    gameObjectToSerialize,
+                    Encoding.UTF8);
+                stream.Seek(0, SeekOrigin.Begin);
+                
+                using (var reader = new StreamReader(stream))
+                {
+                    var rawJson = reader.ReadToEnd();
+                    stream.Seek(0, SeekOrigin.Begin);
+                
+                    var result = _deserializer.Deserialize<IGameObject>(stream);
+                    return Tuple.Create(result, rawJson);
+                }
+            }
+        }
+    }
+}
