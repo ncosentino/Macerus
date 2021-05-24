4a6de840a4f4bad7df5825faa7626bfb0813a126
Nick Cosentino <n.b.cosentino@gmail.com>
2021-05-24 08:37:03 -0700

    ContainerGenerateItemsBehavior Constructor Param

--- Macerus.Plugins.Features.GameObjects.Containers/ContainerGenerateItemsBehavior.cs
+++ Macerus.Plugins.Features.GameObjects.Containers/ContainerGenerateItemsBehavior.cs
@@ -7,9 +7,10 @@ namespace Macerus.Plugins.Features.GameObjects.Containers
         BaseBehavior,
         IContainerGenerateItemsBehavior
     {
-        public ContainerGenerateItemsBehavior(IIdentifier dropTableId)
+        public ContainerGenerateItemsBehavior(IIdentifier dropTableId, bool hasGeneratedItems)
         {
             DropTableId = dropTableId;
+            HasGeneratedItems = hasGeneratedItems;
         }
 
         public IIdentifier DropTableId { get; set; }
