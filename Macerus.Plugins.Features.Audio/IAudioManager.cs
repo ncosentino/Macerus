202b8d50e1a73320c1aa2cc263467c0fb097be64
Nick Cosentino <n.b.cosentino@gmail.com>
2021-06-27 00:03:13 -0700

    RPG-287: Add main menu music
    - Added an IAudioManager interface.

---
+++ Macerus.Plugins.Features.Audio/IAudioManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using ProjectXyz.Api.Framework;
+
+namespace Macerus.Plugins.Features.Audio
+{
+    public interface IAudioManager
+    {
+        Task PlayMusicAsync(IIdentifier musicResourceId);
+    }
+}
