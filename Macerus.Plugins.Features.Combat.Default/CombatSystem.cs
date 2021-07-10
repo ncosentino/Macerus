f4cbd0592f2cb0aa9fb0d8b86a7be7282cdf95c4
Nick Cosentino <n.b.cosentino@gmail.com>
2021-07-09 23:28:56 -0700

    RPG-303: Allow combat end before turn transition (i.e. on killing blow)

--- Macerus.Plugins.Features.Combat.Default/CombatSystem.cs
+++ Macerus.Plugins.Features.Combat.Default/CombatSystem.cs
@@ -2,6 +2,7 @@
 
 using Macerus.Plugins.Features.Combat.Api;
 
+using ProjectXyz.Api.Framework;
 using ProjectXyz.Api.Framework.Entities;
 using ProjectXyz.Api.GameObjects;
 using ProjectXyz.Api.GameObjects.Behaviors;
@@ -21,6 +22,8 @@ namespace Macerus.Plugins.Features.Combat.Default
         private readonly IWinConditionHandlerFacade _winConditionHandler;
         private readonly ILogger _logger;
 
+        private double _nextWinConditionCheckAccumulator;
+
         public CombatSystem(
             ICombatTurnManager combatTurnManager,
             IFilterContextProvider filterContextProvider,
@@ -39,28 +42,42 @@ namespace Macerus.Plugins.Features.Combat.Default
 
         public async Task UpdateAsync(ISystemUpdateContext systemUpdateContext)
         {
-            var turnInfo = systemUpdateContext
-                .GetFirst<IComponent<ITurnInfo>>()
-                .Value;
-            if (!_combatTurnManager.InCombat || turnInfo.ElapsedTurns != 1)
+            if (!_combatTurnManager.InCombat)
             {
                 return;
             }
 
-            var filterContext = _filterContextProvider.GetContext();
-            _combatTurnManager.ProgressTurn(
-                filterContext,
-                1);
+            var turnInfo = systemUpdateContext
+                .GetFirst<IComponent<ITurnInfo>>()
+                .Value;
+            var elapsedTime = systemUpdateContext
+                .GetFirst<IComponent<IElapsedTime>>()
+                .Value;
 
-            if (_winConditionHandler.CheckWinConditions(
-                out var winningTeam,
-                out var losingTeams))
+            if (turnInfo.ElapsedTurns == 1)
             {
-                _turnBasedManager.GlobalSync = true;
-                _turnBasedManager.SyncTurnsFromElapsedTime = true;
-                _combatTurnManager.EndCombat(
-                    winningTeam,
-                    losingTeams);
+                var filterContext = _filterContextProvider.GetContext();
+                _combatTurnManager.ProgressTurn(
+                    filterContext,
+                    1);
+            }
+
+            const double SECONDS_BEFORE_WIN_CHECK = 0.3;
+            _nextWinConditionCheckAccumulator += ((IInterval<double>)elapsedTime.Interval).Value / 1000;
+            if (_nextWinConditionCheckAccumulator >= SECONDS_BEFORE_WIN_CHECK)
+            {
+                _nextWinConditionCheckAccumulator = 0;
+
+                if (_winConditionHandler.CheckWinConditions(
+                    out var winningTeam,
+                    out var losingTeams))
+                {
+                    _turnBasedManager.GlobalSync = true;
+                    _turnBasedManager.SyncTurnsFromElapsedTime = true;
+                    _combatTurnManager.EndCombat(
+                        winningTeam,
+                        losingTeams);
+                }
             }
         }
     }
